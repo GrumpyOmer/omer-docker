197747c65a0f4063dfe02faf2c26cd3bee44dc6b
GrumpyOmer <lovemj353286116@qq.com>
2022-07-19 15:55:08 +0800

    test: test docker-compose restart function

--- golang/main.go
+++ golang/main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
+
+var a = 1
 
 func main() {
 	fmt.Println("this is a golang service")
-	http.HandleFunc("/", HelloHandler)
+	http.HandleFunc("/panic", PanicHandler)
+	http.HandleFunc("/test", TestHandler)
 	http.ListenAndServe(":8765", nil)
 }
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+func PanicHandler(w http.ResponseWriter, r *http.Request) {
+	// 测试守护进程
+	a := make(map[int]int)
+	go func() {
+		for i := 0; i < 100; i++ {
+			a[i] = i
+		}
+	}()
+	go func() {
+		for i := 0; i < 100; i++ {
+			a[i] = i
+		}
+	}()
+	fmt.Println(111)
+}
+
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	a++
+	fmt.Fprintf(w, strconv.Itoa(a))
 }
